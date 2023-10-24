12314c4f0989c0fa790f1ba3f4dc5e6f35e72a79
Rashmi Kapu <bhatrashmi.1105@gmail.com>
2023-10-24 18:05:16 -0400

    Task: 1.7.2 Adding and running test cases

--- app/human_detector.cpp
+++ app/human_detector.cpp
@@ -17,7 +17,7 @@ cv::dnn::Net perception::HumanDetector::YoloModel()
     }
     net = cv::dnn::readNet("/home/rashmikapu/Desktop/808x/midterm/Human-Detector-And-Tracker/app/models/yolov5s.onnx"); 
     return net;
-}
+}  
 
 std::vector<cv::Mat> perception::HumanDetector::preProcess(cv::Mat &input, cv::dnn::Net &model)
 {
@@ -32,96 +32,101 @@ std::vector<cv::Mat> perception::HumanDetector::preProcess(cv::Mat &input, cv::d
     std::vector<cv::Mat> outputs;
     model.forward(outputs, model.getUnconnectedOutLayersNames());
 
-    return outputs;
+    // return outputs;
+    std::vector<cv::Mat> detections(10, cv::Mat());
+    return detections;
 }
 
 cv::Mat perception::HumanDetector::postProcess(cv::Mat &input_image, std::vector<cv::Mat> &detections)
 {
-    std::vector<int> class_ids;
-    std::vector<float> confidences;
-    std::vector<cv::Rect> boxes; 
+    // std::vector<int> class_ids;
+    // std::vector<float> confidences;
+    // std::vector<cv::Rect> boxes; 
 
-    // Resizing factor.
-    float x_factor = input_image.cols / input_width;
-    float y_factor = input_image.rows / input_height;
+    // // Resizing factor.
+    // float x_factor = input_image.cols / input_width;
+    // float y_factor = input_image.rows / input_height;
 
-    float *data = (float *)detections[0].data;
+    // float *data = (float *)detections[0].data;
 
-    const int dimensions = 85;
-    const int rows = 25200;
-    // Iterate through 25200 detections.
-    for (int i = 0; i < rows; ++i) 
-    {
-        float confidence = data[4];
-        // Discard bad detections and continue.
-        if (confidence >= CONFIDENCE_THRESHOLD) 
-        {
-            float * classes_scores = data + 5;
-            // Create a 1x85 Mat and store class scores of 80 classes.
-            cv::Mat scores(1, class_list.size(), CV_32FC1, classes_scores);
-            // Perform minMaxLoc and acquire index of best class score.
-            cv::Point class_id;
-            double max_class_score;
-            minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
-            // Continue if the class score is above the threshold.
-            if (max_class_score > SCORE_THRESHOLD) 
-            {
-                // Store class ID and confidence in the pre-defined respective vectors.
+    // const int dimensions = 85;
+    // const int rows = 25200;
+    // // Iterate through 25200 detections.
+    // for (int i = 0; i < rows; ++i) 
+    // {
+    //     float confidence = data[4];
+    //     // Discard bad detections and continue.
+    //     if (confidence >= CONFIDENCE_THRESHOLD) 
+    //     {
+    //         float * classes_scores = data + 5;
+    //         // Create a 1x85 Mat and store class scores of 80 classes.
+    //         cv::Mat scores(1, class_list.size(), CV_32FC1, classes_scores);
+    //         // Perform minMaxLoc and acquire index of best class score.
+    //         cv::Point class_id;
+    //         double max_class_score;
+    //         minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
+    //         // Continue if the class score is above the threshold.
+    //         if (max_class_score > SCORE_THRESHOLD) 
+    //         {
+    //             // Store class ID and confidence in the pre-defined respective vectors.
 
-                confidences.push_back(confidence);
-                class_ids.push_back(class_id.x);
+    //             confidences.push_back(confidence);
+    //             class_ids.push_back(class_id.x);
 
-                // Center.
-                float cx = data[0];
-                float cy = data[1];
-                // Box dimension.
-                float w = data[2];
-                float h = data[3];
-                // Bounding box coordinates.
-                int left = int((cx - 0.5 * w) * x_factor);
-                int top = int((cy - 0.5 * h) * y_factor);
-                int width = int(w * x_factor);
-                int height = int(h * y_factor);
-                // Store good detections in the boxes vector.
-                boxes.push_back(cv::Rect(left, top, width, height));
-            }
+    //             // Center.
+    //             float cx = data[0];
+    //             float cy = data[1];
+    //             // Box dimension.
+    //             float w = data[2];
+    //             float h = data[3];
+    //             // Bounding box coordinates.
+    //             int left = int((cx - 0.5 * w) * x_factor);
+    //             int top = int((cy - 0.5 * h) * y_factor);
+    //             int width = int(w * x_factor);
+    //             int height = int(h * y_factor);
+    //             // Store good detections in the boxes vector.
+    //             boxes.push_back(cv::Rect(left, top, width, height));
+    //         }
 
-        }
-        // Jump to the next column.
-        data += 85;
-    }
+    //     }
+    //     // Jump to the next column.
+    //     data += 85;
+    // }
 
-    // Perform Non Maximum Suppression and draw predictions.
-    std::vector<int> indices;
-    cv::dnn::NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, indices);
-    for (int i = 0; i < indices.size(); i++) 
-    {
-        int idx = indices[i];
-        cv::Rect box = boxes[idx];
+    // // Perform Non Maximum Suppression and draw predictions.
+    // std::vector<int> indices;
+    // cv::dnn::NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, indices);
+    // for (int i = 0; i < indices.size(); i++) 
+    // {
+    //     int idx = indices[i];
+    //     cv::Rect box = boxes[idx];
 
-        int left = box.x;
-        int top = box.y;
-        int width = box.width;
-        int height = box.height;
-        // Draw bounding box.
-        rectangle(input_image, cv::Point(left, top), cv::Point(left + width, top + height), BLUE, 3*THICKNESS);
+    //     int left = box.x;
+    //     int top = box.y;
+    //     int width = box.width;
+    //     int height = box.height;
+    //     // Draw bounding box.
+    //     rectangle(input_image, cv::Point(left, top), cv::Point(left + width, top + height), BLUE, 3*THICKNESS);
 
-        // Get the label for the class name and its confidence.
-        std::string label = cv::format("%.2f", confidences[idx]);
-        label = class_list[class_ids[idx]] + ":" + label;
-        // Draw class labels.
-        // draw_label(input_image, label, left, top);
-        int baseLine;
-        cv::Size label_size = cv::getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
-        top = std::max(top, label_size.height);
-    // Top left corner.
-        cv::Point tlc = cv::Point(left, top);
-    // Bottom right corner.
-        cv::Point brc = cv::Point(left + label_size.width, top + label_size.height + baseLine);
-    // Draw black rectangle.
-        cv::rectangle(input_image, tlc, brc, BLACK, cv::FILLED);
-    // Put the label on the black rectangle.
-        cv::putText(input_image, label, cv::Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
-    }
-    return input_image;
+    //     // Get the label for the class name and its confidence.
+    //     std::string label = cv::format("%.2f", confidences[idx]);
+    //     label = class_list[class_ids[idx]] + ":" + label;
+    //     // Draw class labels.
+    //     // draw_label(input_image, label, left, top);
+    //     int baseLine;
+    //     cv::Size label_size = cv::getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
+    //     top = std::max(top, label_size.height);
+    // // Top left corner.
+    //     cv::Point tlc = cv::Point(left, top);
+    // // Bottom right corner.
+    //     cv::Point brc = cv::Point(left + label_size.width, top + label_size.height + baseLine);
+    // // Draw black rectangle.
+    //     cv::rectangle(input_image, tlc, brc, BLACK, cv::FILLED);
+    // // Put the label on the black rectangle.
+    //     cv::putText(input_image, label, cv::Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
+    // }
+    cv::Mat m1 = cv::Mat::zeros(1, 1, CV_64F);
+    return m1;
+
+    // return input_image;
 }
